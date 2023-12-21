764304dc31afeb34538f610e761acdd927d5865b
washingtonlzc <washingtonlzc@gmail.com>
2023-12-21 16:19:51 -0300

    adicionando gifs para ilustrar

--- classejogo.php
+++ classejogo.php
@@ -2,46 +2,48 @@
 
 class Heroi
 {
-    //Definindo a classe do heroi
     public $nome;
     public $idade;
     public $tipo;
-
     public function __construct($nome, $idade, $tipo)
     {
         $this->nome = $nome;
         $this->idade = $idade;
-        $this->tipo = $tipo;
-    }
+        $this->tipo = $tipo;    }
 
     public function atacar()
     {
         switch ($this->tipo) {
             case 'mago':
-                $ataque = 'usou magia';
+                $ataque = 'usando magia';
                 break;
             case 'guerreiro':
-                $ataque = 'usou espada';
+                $ataque = 'usando espada';
                 break;
             case 'monge':
-                $ataque = 'usou artes marciais';
+                $ataque = 'usando artes marciais';
                 break;
             case 'ninja':
-                $ataque = 'usou shuriken';
+                $ataque = 'usando shuriken';
                 break;
             default:
-                $ataque = 'usou um ataque indefinido';
+                $ataque = 'usando um ataque indefinido';
                 break;
         }
 
-        return "O {$this->tipo} atacou usando {$ataque}";
+        return "O {$this->tipo} {$this->nome}, com {$this->idade} anos, atacou usando {$ataque}";
     }
 }
+$heroName = isset($_POST['heroName']) ? $_POST['heroName'] : 'Herói'; // Valor padrão se o nome não for fornecido
+$heroAge = isset($_POST['heroAge']) ? $_POST['heroAge'] : 25; // Valor padrão se a idade não for fornecida
+$heroType = isset($_POST['heroType']) ? $_POST['heroType'] : 'mago'; // Valor padrão se o tipo não for fornecido
 
-// Criação de um herói (pode ser personalizado conforme necessário)
-$heroi = new Heroi('Herói', 25, 'mago');
+// Criação de um herói com as informações fornecidas
+$heroi = new Heroi($heroName, $heroAge, $heroType);
+
 // Realiza o ataque e retorna a mensagem
 $mensagem = $heroi->atacar();
+
 // Resposta em formato JSON
 $response = ['message' => $mensagem];
 echo json_encode($response);
--- index.php
+++ index.php
@@ -1,27 +1,52 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jogo de Heróis</title>
+    <title>Ataque de Herois</title>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 </head>
 <body>
 
+<label for="heroName">Nome do Herói:</label>
+<input type="text" id="heroName" name="heroName">
+
+<label for="heroAge">Idade do Herói:</label>
+<input type="text" id="heroAge" name="heroAge">
+
+<label for="heroType">Tipo do Herói:</label>
+<select id="heroType" name="heroType">
+    <option value="mago">Mago</option>
+    <option value="guerreiro">Guerreiro</option>
+    <option value="monge">Monge</option>
+    <option value="ninja">Ninja</option>
+</select>
+
+<button id="attackButton">Atacar</button>
 <div id="output"></div>
+<div id="gifContainer"></div>
 
 <script>
     $(document).ready(function () {
-        $.ajax({
-            type: "POST",
-            url: "classejogo.php",
-            dataType: "json",
-            success: function (response) {
-                $("#output").html(response.message);
-            }
+        $("#attackButton").on("click", function () {
+            var heroName = $("#heroName").val();
+            var heroAge = $("#heroAge").val();
+            var heroType = $("#heroType").val();
+            $.ajax({
+                type: "POST",
+                url: "classejogo.php",
+                data: {heroName: heroName, heroAge: heroAge, heroType: heroType},
+                dataType: "json",
+                success: function (response) {
+                    $("#output").html(response.message);
+
+                    // Exibir o gif correspondente
+                    var gifPath = "assets/" + heroType + ".gif";
+                    $("#gifContainer").html("<img src='" + gifPath + "' alt='" + heroType + "'>");
+                }
+            });
         });
     });
 </script>
-
 </body>
 </html>
